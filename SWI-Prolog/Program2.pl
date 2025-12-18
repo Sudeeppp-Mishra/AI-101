960e1a5b4123069d72d47c53dea0844f1d2fa583
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-12-18 17:36:26 +0545

    feat: add queries to be run in comments

--- SWI-Prolog/Program2.pl
+++ SWI-Prolog/Program2.pl
@@ -15,3 +15,15 @@ difficulty(circuits, hard).
 classinformation(Day, Class, Diff) :-
     schedule(Day, Class),
     difficulty(Class, Diff).
+
+% Now run the following Queries to check the results
+%
+% 1. schedule(monday,programming).
+% 2. schedule(monday, english).
+% 3. difficulty( programming, easy).
+% 4. difficulty(programming, hard).
+% 5. classinformation(tuesday, Class, Diff).
+% 6. classinformation(tuesday, Class, easy).
+% 7. classinformation(Day, Class, hard).
+% 8. classinformation(Day, english, hard).
+% 9. classinformation(Day, english, easy).
